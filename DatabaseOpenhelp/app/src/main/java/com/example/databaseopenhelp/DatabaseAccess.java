3f2988893a88696300f43398ce21044d6dc8785b
Jeong jaemin <jmjung1997@gmail.com>
2022-05-26 20:50:23 +0900

    sqlite3 file-output success

--- DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/DatabaseAccess.java
+++ DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/DatabaseAccess.java
@@ -6,38 +6,43 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseAccess {
-   public SQLiteOpenHelper openHelper;
-   public SQLiteDatabase db;
-   public static DatabaseAccess instance;
-   Cursor c=null;
-public DatabaseAccess(Context context){
-    this.openHelper=new DatabaseOpenHelper(context);
+    public SQLiteOpenHelper openHelper;
+    public SQLiteDatabase db;
+    public static DatabaseAccess instance;
+    Cursor c = null;
 
-}
-public static DatabaseAccess getInstance(Context context){
-    if(instance==null){
-        instance=new DatabaseAccess(context);
+    public DatabaseAccess(Context context) {
+        this.openHelper = new DatabaseOpenHelper(context);
 
     }
-    return instance;
-}
-public void open(){
-    this.db=openHelper.getWritableDatabase();
 
-}
-public void close(){
-    if(db!=null){
-        this.db.close();
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context);
+
+        }
+        return instance;
     }
-}
-public String getAddress(String name){
-    c=db.rawQuery("select Address from Table1 where Name ='"+name+"'",new String[]{});
-    StringBuffer buffer= new StringBuffer();
-    while(c.moveToNext()){
-        String address=c.getString(0);
-        buffer.append(""+address);
+
+    public void open() {
+        this.db = openHelper.getWritableDatabase();
 
     }
-    return buffer.toString();
-}
+
+    public void close() {
+        if (db != null) {
+            this.db.close();
+        }
+    }
+
+    public String getAddress(String name) {
+        c = db.rawQuery("select menu from recipe where ingre1 ='" + name + "'", new String[]{});
+        StringBuffer buffer = new StringBuffer();
+        while (c.moveToNext()) {
+            String address = c.getString(0);
+            buffer.append("" + address);
+
+        }
+        return buffer.toString();
+    }
 }
