c167084c2c763eaf7ce3609de1cc1509cc0dcd47
jaemin <jmjung1997@gmail.com>
2022-05-31 15:41:38 +0900

    5.31

--- DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/DatabaseAccess.java
+++ DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/DatabaseAccess.java
@@ -11,12 +11,12 @@ public class DatabaseAccess {
     public static DatabaseAccess instance;
     Cursor c = null;
 
-    public DatabaseAccess(Context context) {
+    public DatabaseAccess(Context context) { //데이터 베이스 생성자
         this.openHelper = new DatabaseOpenHelper(context);
 
     }
 
-    public static DatabaseAccess getInstance(Context context) {
+    public static DatabaseAccess getInstance(Context context) { // 데이터 베이스 객체 생성 함수
         if (instance == null) {
             instance = new DatabaseAccess(context);
 
@@ -24,23 +24,23 @@ public class DatabaseAccess {
         return instance;
     }
 
-    public void open() {
+    public void open() {//데이터베이스 읽을 수 있는 권한 부여 함수
         this.db = openHelper.getWritableDatabase();
 
     }
 
-    public void close() {
+    public void close() {//데이터 베이스 다 읽고 닫아주는 함수
         if (db != null) {
             this.db.close();
         }
     }
 
-    public String getAddress(String name) {
-        c = db.rawQuery("select menu from recipe where ingre1 ='" + name + "'", new String[]{});
+    public String getmenu(String name) { //재료를 name 매개변수로 받으면 query문을 통해 menu를 출력해주는 함수
+        c = db.rawQuery("select menu from recipe where ingre1 ='" + name + "'" , new String[]{});
         StringBuffer buffer = new StringBuffer();
         while (c.moveToNext()) {
-            String address = c.getString(0);
-            buffer.append("" + address);
+            String menu = c.getString(0);
+            buffer.append("" + menu);
 
         }
         return buffer.toString();
--- DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/MainActivity.java
+++ DatabaseOpenhelp/app/src/main/java/com/example/databaseopenhelp/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     public EditText name;
     public Button query_button;
-    public TextView result_address;
+    public TextView result_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +19,24 @@ public class MainActivity extends AppCompatActivity {
 
         name=findViewById(R.id.name);
         query_button=findViewById(R.id.query_button);
-        result_address=findViewById(R.id.result);
+        result_menu=findViewById(R.id.result);
 
         query_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String menu;
                 DatabaseAccess databaseAccess=DatabaseAccess.getInstance(getApplicationContext());
                 databaseAccess.open();
 
-                String n=name.getText().toString();
-                String address=databaseAccess.getAddress(n);
+                String n=name.getText().toString(); //getText()가 에디터블 인터페이스 타입 형태로 호출 되기 때문에 toString을 써서 문자열 타입으로 바꿔줌
 
-                result_address.setText(address);
+                String[] new_n = n.split("\\s+");// 연속으로 값을 입력 받을 때 공백을 기준으로 string을 구별해줌 ex) n=사과 바나나-> n1=사과 n2=바나나
+                for(int i=0; i< new_n.length;i++ ) {
+                    menu = databaseAccess.getmenu(new_n[i]);
+                    result_menu.setText(menu);
+                }
+
+
                 databaseAccess.close();
             }
         });
